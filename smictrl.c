1fd4e2196552c7385df61c7f9cc93a1c7e4fd12b
John Morris <jman@mk0.zultron.com>
2016-03-20 11:18:08 -0500

    Commit changes from Marty Vona

--- smictrl.c
+++ smictrl.c
@@ -1,7 +1,19 @@
-/*
+/* -*- c-basic-offset: 4 -*-
+ *
  * Copyright (C) 2006, 2010 Jan Kiszka <jan.kiszka@web.de>.
  *
+ * -g, -v, -d, -m, -c options by Marty Vona <martyvona@gmail.com>
+ *
+ * -g option based on https://bugzilla.kernel.org/attachment.cgi?id=23059
+ *
  * Based on Xenomai's SMI workaround.
+ *
+ * If there is an accompanying Makefile, just run "make".  Otherwise the
+ * suggested build commands on a Debian-like system are:
+ *
+ * sudo apt-get install libpci-dev
+ * KSRC=/lib/modules/`uname -r`/source
+ * cc -O2 -Wall -I $KSRC/include -lz -lpci smictrl.c -o smictrl
  *
  * smictrl is free software; you can redistribute it and/or modify it
  * under the terms of the GNU General Public License as published by
@@ -18,6 +30,7 @@
 
 #include <pci/pci.h>
 
+/* Intel chipset LPC (Low Pin Count) bus controller: PCI device=31 function=0 */
 #define LPC_DEV             31
 #define LPC_FUNC            0
 
@@ -26,6 +39,7 @@
 
 #define SMI_CTRL_ADDR       0x30
 #define SMI_STATUS_ADDR     0x34
+#define SMI_ALT_GPIO_ADDR   0x38
 #define SMI_MON_ADDR        0x40
 
 /* SMI_EN register: ICH[0](16 bits), ICH[2-5](32 bits) */
@@ -41,14 +55,38 @@
 #define BIOS_EN_BIT         (0x01 << 2)
 #define GBL_SMI_EN_BIT      (0x01) /* This is reset by a PCI reset event! */
 
-static uint16_t get_smi_en_addr(struct pci_dev *dev)
+static uint16_t get_smi_en_addr(struct pci_dev *dev, uint8_t gpio)
 {
     uint8_t byte0, byte1;
 
     byte0 = pci_read_byte(dev, PMBASE_B0);
     byte1 = pci_read_byte(dev, PMBASE_B1);
 
-    return SMI_CTRL_ADDR + (((byte1 << 1) | (byte0 >> 7)) << 7); // bits 7-15
+    return ((gpio) ? SMI_ALT_GPIO_ADDR : SMI_CTRL_ADDR) + 
+        (((byte1 << 1) | (byte0 >> 7)) << 7); // bits 7-15
+}
+
+static void print_bits(FILE *stream, uint32_t *val) {
+  
+#define PRINT_BIT(f) {                                              \
+        fprintf(stream, "%20s (0x%08x)", #f, f);                    \
+        if (val) fprintf(stream, " = %s", ((*val)&f) ? "1" : "0");  \
+        fprintf(stream, "\n");                                      \
+    }
+    
+    PRINT_BIT(INTEL_USB2_EN_BIT);
+    PRINT_BIT(LEGACY_USB2_EN_BIT);
+    PRINT_BIT(PERIODIC_EN_BIT);
+    PRINT_BIT(TCO_EN_BIT);
+    PRINT_BIT(MCSMI_EN_BIT);
+    PRINT_BIT(SWSMI_TMR_EN_BIT);
+    PRINT_BIT(APMC_EN_BIT);
+    PRINT_BIT(SLP_EN_BIT);
+    PRINT_BIT(LEGACY_USB_EN_BIT);
+    PRINT_BIT(BIOS_EN_BIT);
+    PRINT_BIT(GBL_SMI_EN_BIT);
+    
+#undef PRINT_BIT
 }
 
 int main(int argc, char *argv[])
@@ -58,24 +96,59 @@ int main(int argc, char *argv[])
     struct pci_access *pacc;
     struct pci_dev *dev;
     uint16_t smi_en_addr;
-    uint32_t new_value = 0;
-    int c, set_value = 0;
+    uint32_t set_bits = 0, clr_bits = 0, opt_bits = 0;
+    uint32_t orig_value, new_value, new_new_value;
+    int c, set_value = 0, dry = 0, gpio = 0, verb = 0;
+    const char *reg_name = "SMI_EN";
+    int reg_width = 8; /* nybbles */
 
-    while ((c = getopt(argc,argv,"s:")) != EOF)
+    while ((c = getopt(argc,argv,"hdgvs:m:c:")) != EOF)
         switch (c) {
-            case 's':
-                set_value = 1;
-                new_value = strtol(optarg, NULL,
-                    (strncmp(optarg, "0x", 2) == 0) ? 16 : 10);
+
+            case 'd':
+                dry = 1;
                 break;
 
-            default:
-                fprintf(stderr, "usage: smictrl [-s <value>]\n");
+            case 'g':
+                gpio = 1;
+                reg_name = "alt GPIO SMI_EN";
+                reg_width = 4;
+                break;
+
+            case 'v':
+                verb = 1;
+                break;
+
+            case 's': case 'm': case 'c':
+                set_value = 1;
+                opt_bits = strtol(optarg, NULL,
+                                  (strncmp(optarg, "0x", 2) == 0) ? 16 : 10);
+                if (c == 'm') { set_bits |= opt_bits; clr_bits &= ~opt_bits; }
+                if (c == 'c') { clr_bits |= opt_bits; set_bits &= ~opt_bits; }
+                else          { set_bits  = opt_bits; clr_bits  = ~opt_bits; }
+                break;
+
+            case 'h': default:
+                fprintf(stderr, "usage: smictrl [-h] [-d] [-s <bits>] "
+                                               "[-m <bits>] [-c <bits>]\n");
+                fprintf(stderr, "  <bits> are in decimal or 0xHEX\n");
+                fprintf(stderr, "  -s sets all bits\n");
+                fprintf(stderr, "  -m marks (sets) individual bits\n");
+                fprintf(stderr, "  -c clears individual bits\n");
+                fprintf(stderr, "  -g operate on alternate GPIO SMI_EN\n");
+                fprintf(stderr, "  -v show individual bits\n");
+                fprintf(stderr, "  -d dry run\n");
+                fprintf(stderr, "  multiple options are combined in order\n");
+                fprintf(stderr, "  common SMI_EN register bits "
+                                "(not for alternate GPIO):\n");
+                print_bits(stderr, 0);
                 exit(2);
         }
 
+    printf(" attemting to read %s - run with -h for help\n", reg_name);
+
     if (iopl(3) < 0) {
-        printf("root permissions required\n");
+        printf(" root permissions required\n");
         exit(1);
     }
 
@@ -96,16 +169,46 @@ int main(int argc, char *argv[])
         pci_lookup_name(pacc, device_name, sizeof(device_name),
                         PCI_LOOKUP_DEVICE, dev->vendor_id, dev->device_id);
 
-        printf("SMI-enabled chipset found:\n %s %s (%04x:%04x)\n",
+        printf(" SMI-enabled chipset found:\n %s %s (%04x:%04x)\n",
                vendor_name, device_name, dev->vendor_id, dev->device_id);
 
-        smi_en_addr = get_smi_en_addr(dev);
+        smi_en_addr = get_smi_en_addr(dev, gpio);
 
-        printf(" SMI_EN register:\t%08x\n", inl(smi_en_addr));
+        if (!gpio) orig_value = inl(smi_en_addr);
+        else orig_value = inw(smi_en_addr);
+
+        printf(" %s register current value:\t0x%0*x\n", 
+               reg_name, reg_width, orig_value);
+
+        if (!gpio && verb) print_bits(stdout, &orig_value);
 
         if (set_value) {
-            outl(new_value, smi_en_addr);
-            printf(" new value:\t\t%08x\n", inl(smi_en_addr));
+
+            new_value = orig_value;
+            new_value |= set_bits;
+            new_value &= ~clr_bits;
+
+            printf(" %s set %s to value:\t0x%0*x\n",
+                   (dry) ? "(dry run) would" : "attempting to",
+                   reg_name, reg_width, new_value);
+
+            if (!gpio && verb) print_bits(stdout, &new_value);
+
+            if (!dry) {
+                if (!gpio) outl(new_value, smi_en_addr);
+                else outw(new_value, smi_en_addr);
+
+                if (!gpio) new_new_value = inl(smi_en_addr);
+                else new_new_value = inw(smi_en_addr);
+
+                printf(" %s register new value:\t0x%0*x\n", 
+                       reg_name, reg_width, new_new_value);
+
+                if (!gpio && verb) print_bits(stdout, &new_new_value);
+            }
+
+        } else {
+            printf(" %s register unchanged\n", reg_name);
         }
 
         goto out;
